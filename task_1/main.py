e89a1a004e4adc86a49cd0f817b84d4712980ce9
Ananasy <anastasiya_johnni@mail.ru>
2017-08-31 19:16:17 +0300

    use argparse

--- task_1/main.py
+++ task_1/main.py
@@ -1,28 +1,32 @@
-import sys
+import matplotlib.pyplot as plt
+import argparse
 
 import helper
-import matplotlib.pyplot as plt
-import numpy as np
 import hst
+
+
+parser = argparse.ArgumentParser(description='Build histogram for given array.')
+parser.add_argument('-f', metavar='filename', type=str, nargs=1,
+                    help='path to file with data')
+parser.add_argument('-b', metavar='num_of_bins', type=int, nargs=1,
+                    help='number of bins')
 
 
 def run():
 
-    assert len(sys.argv) == 3, "Wrong number of arguments"
-
-    filename = sys.argv[1]
-    beans_num = sys.argv[2]
+    args = parser.parse_args()
+    args_dict = vars(args)
+    filename = args_dict['f'][0]
+    beans_num = args_dict['b'][0]
     data = helper.read_file(filename)
 
     hist_val = hst.get_hist(data, len(data), int(beans_num))
 
+    # visualisation
     plt.bar(hist_val['x_val'][:-1], hist_val['y_val'], hist_val['int_size'], color='m')
     plt.xlabel('Bins')
     plt.ylabel('Frequency')
     plt.show()
-
-    print(hist_val['x_val'], hist_val['y_val'])
-    print(np.histogram(data, int(beans_num)))
 
 if __name__ == '__main__':
     run()
